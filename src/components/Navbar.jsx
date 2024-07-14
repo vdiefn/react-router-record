6f2d0f325ec60213609aa6976ff012d5bd0b6f74
Ellen <ddhkxxhxx@hotmail.com>
2024-07-14 15:06:59 +0800

    setting complete

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+
+function Navbar() {
+  return (<nav className="navbar navbar-expand-lg bg-light">
+    <div className="container-fluid">
+      <a className="navbar-brand" href="#">Navbar</a>
+
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+        </ul>  
+      </div>
+    </div>
+  </nav>
+
+  
+
+  )
+}
+
+export default Navbar
---
+++ src/pages/About.jsx
@@ -0,0 +1,9 @@
+function About() {
+  return (
+    <div>
+      這是關於我的頁面
+    </div>
+  )
+}
+
+export default About

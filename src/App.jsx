9df522cb9780d40c2c0d3dd4ebf809e725c0ad44
Ellen <ddhkxxhxx@hotmail.com>
2024-07-17 11:50:24 +0800

    add routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
 
 function App() {
-
-
   return (
     <div>
       <Navbar />
       <div className="container mt-3">
-        123
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/about' element={<About />}></Route>
+        </Routes>
+        
       </div>
     </div>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 
 function Navbar() {
   return (<nav className="navbar navbar-expand-lg bg-light">
@@ -7,7 +8,10 @@ function Navbar() {
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
           <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="#">Home</a>
+            <Link className="nav-link active" aria-current="page" to='/'>Home</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link active" aria-current="page" to='/about'>About</Link>
           </li>
         </ul>  
       </div>

159b9fad75f805754ceab1de9ced241aa5f7bcf0
Ellen <ddhkxxhxx@hotmail.com>
2024-07-19 13:01:32 +0800

    Navbar refactor

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 function Navbar() {
   return (<nav className="navbar navbar-expand-lg bg-light">
@@ -8,13 +8,13 @@ function Navbar() {
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
           <li className="nav-item">
-            <Link className="nav-link active" aria-current="page" to='/'>Home</Link>
+            <NavLink className="nav-link" aria-current="page" to='/'>Home</NavLink>
           </li>
           <li className="nav-item">
-            <Link className="nav-link active" aria-current="page" to='/about'>About</Link>
+            <NavLink className="nav-link" aria-current="page" to='/about'>About</NavLink>
           </li>
           <li className="nav-item">
-            <Link className="nav-link active" aria-current="page" to='/album'>相簿</Link>
+            <NavLink className="nav-link" aria-current="page" to='/album'>相簿</NavLink>
           </li>
         </ul>  
       </div>

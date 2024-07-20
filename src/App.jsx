81de091ae33dae213451c5d93a822417ae30956b
Ellen <ddhkxxhxx@hotmail.com>
2024-07-20 10:51:56 +0800

    import refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar'
 import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
-import AlbumLayout from './pages/AlbumLayout'
-import AlbumIndex from './pages/AlbumIndex'
-import AlbumPhoto from './pages/AlbumPhoto'
-import AlbumSearch from './pages/AlbumSearch'
-import NotFound from './pages/NotFound'
+import { Home, About, AlbumLayout, AlbumIndex, AlbumPhoto, AlbumSearch, NotFound } from './pages'
+
 
 function App() {
   return (
